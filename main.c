4cdef3b897ba7c152448a9398c9bdce4fe478a38
csalhabb <charbel.salhab.2003@gmail.com>
2023-09-27 19:27:26 +0200

    fix some issues on menu causing segmentation faults + add system clear

--- main.c
+++ main.c
@@ -1,11 +1,19 @@
+/*
+Created by : Charbel Salhab, Olivier Nart, Bao Nguyen Tran Duc
+Date : 27/09/2023
+Projet DoomdepthC
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "menu/menu.c"
 #include "Game.c"
 
 int main() {
-    Player * player = new_player("Andre");
-    weapon_starter_selection(player);
-    Battle * battle = init_simple_battle(player);
-    wave_battle(battle);
+      display_menu();
+//    Player * player = new_player("Andre");
+//    weapon_starter_selection(player);
+//    Battle * battle = init_simple_battle(player);
+//    wave_battle(battle);
 }
--- menu/menu.c
+++ menu/menu.c
@@ -5,53 +5,79 @@ Projet DoomdepthC
 */
 
 #include "../headers/menu.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
-void display_menu(){
+void display_menu() {
+    system("clear");
 
-    char *name;
+    char name[100];
 
-    printf("Hello ! What's your name ? ");
+    printf("Hello! What's your name? ");
     scanf("%s", name);
 
-    while(1){
+    system("clear");
 
+    printf("Hello, %s! Welcome to Doomdepth!\n", name);
+
+    while (1) {
         printf("\nMenu\n");
-        printf("0. Show Stats : \n");
-        printf("1. Fight !\n");
-        printf("2. Choose spells !\n");
-        printf("3. Check Characters : \n");
+        printf("0. Show Stats\n");
+        printf("1. Fight\n");
+        printf("2. Choose spells\n");
+        printf("3. Check Characters\n");
         printf("4. Exit the game\n");
 
         int choice;
 
-        printf("Enter your choice :  ");
-        scanf("%d",&choice);
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
 
-        switch(choice){
+        switch (choice) {
 
             case 0:
-                //
+                system("clear");
+                // show stats
                 break;
 
             case 1:
-
+                system("clear");
+                // attack
                 break;
 
             case 2:
-
+                system("clear");
+                // choose spells
                 break;
 
             case 3:
-
+                system("clear");
+                // check characters
                 break;
 
             case 4:
-                printf("\nGoodbye !\n");
-                exit(0);
+                system("clear");
+                // exit the program
+                printf("Are you sure? ");
+                char decision;
+                scanf(" %c", &decision);
+
+                decision = toupper(decision);
+
+                if (decision == 'Y') {
+                    printf("\nGoodbye!\n");
+                    exit(0);
+                } else if (decision == 'N') {
+                    system("clear");
+                } else {
+                    system("clear");
+                    printf("You have to choose between yes (Y) or no (N)!\n");
+                }
+                break;
+
             default:
-                printf("\nChoose between 1 and 4 !\n");
-
+                printf("\nChoose between 0 and 4!\n");
         }
-
     }
 }
