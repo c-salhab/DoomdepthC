1e2b920071c4abba1824f4cee6b5ddb7e95d57c1
c-salhab <charbel.salhab.2003@gmail.com>
2023-10-07 10:32:31 +0200

    set, get, select heal spell

--- spells/spells.c
+++ spells/spells.c
@@ -59,35 +59,35 @@ void set_offensive(Character *character, char *selection) {
 void select_offensive_spell(Character *character) {
     system("clear");
 
-    char *offensiveSpell[] = {"Dragon Breath", "Eat This", "Lightning Chain"};
-    int quantity = sizeof(offensiveSpell) / sizeof(offensiveSpell[0]);
+    char *offensive_spell[] = {"Dragon Breath", "Eat This", "Lightning Chain"};
+    int quantity = sizeof(offensive_spell) / sizeof(offensive_spell[0]);
 
-    char *offensive_spell = get_offensive(character);
-    printf("Your current offensive spell : %s\n", offensive_spell);
+    char *offensive = get_offensive(character);
+    printf("Your current offensive spell : %s\n", offensive);
     printf("\n");
 
     for (int i = 0; i < quantity; i++) {
-        printf("%d. %s\n", i + 1, offensiveSpell[i]);
+        printf("%d. %s\n", i + 1, offensive_spell[i]);
     }
 
     printf("\n");
 
     int choice;
 
-    printf("Enter Your Choice (or 0 to cancel): ");
+    printf("Enter your choice (or 0 to cancel): ");
     scanf("%d", &choice);
 
     system("clear");
 
     if (choice >= 1 && choice <= quantity) {
         // printf("%s", offensiveSpell[choice - 1]);
-        set_offensive(character, offensiveSpell[choice - 1]);
-        offensive_spell = get_offensive(character);
+        set_offensive(character, offensive_spell[choice - 1]);
+        offensive = get_offensive(character);
     } else if (choice != 0) {
         printf("invalid choice: %d\n", choice);
     }
 
-    free(offensive_spell);
+    free(offensive);
 }
 
 char *get_defensive(Character *character) {
@@ -144,35 +144,120 @@ void set_defensive(Character *character, char *selection) {
 void select_defensive_spell(Character *character) {
     system("clear");
 
-    char *defensiveSpell[] = {"Dragon Skin", "Protected Area", "Stick To Me"};
-    int quantity = sizeof(defensiveSpell) / sizeof(defensiveSpell[0]);
+    char *defensive_spell[] = {"Dragon Skin", "Protected Area", "Stick To Me"};
+    int quantity = sizeof(defensive_spell) / sizeof(defensive_spell[0]);
 
-    char *defensive_spell = get_defensive(character);
-    printf("Your current offensive spell : %s\n", defensive_spell);
+    char *defensive = get_defensive(character);
+    printf("Your current defensive spell : %s\n", defensive);
     printf("\n");
 
     for (int i = 0; i < quantity; i++) {
-        printf("%d. %s\n", i + 1, defensiveSpell[i]);
+        printf("%d. %s\n", i + 1, defensive_spell[i]);
     }
 
     printf("\n");
 
     int choice;
 
-    printf("Enter Your Choice (or 0 to cancel): ");
+    printf("Enter your choice (or 0 to cancel): ");
     scanf("%d", &choice);
 
     system("clear");
 
     if (choice >= 1 && choice <= quantity) {
-        // printf("%s", defensiveSpell[choice - 1]);
-        set_defensive(character, defensiveSpell[choice - 1]);
-        defensive_spell = get_defensive(character);
+        // printf("%s", defensive_spell[choice - 1]);
+        set_defensive(character, defensive_spell[choice - 1]);
+        defensive = get_defensive(character);
     } else if (choice != 0) {
         printf("invalid choice: %d\n", choice);
     }
 
-    free(defensive_spell);
+    free(defensive);
+}
+
+char *get_heal(Character *character) {
+
+    if (character->heal_spell == NULL || character->heal_spell->spell_name == NULL) {
+        return strdup("No heal spell");
+    } else {
+        return strdup(character->heal_spell->spell_name);
+    }
+
+}
+
+void set_heal(Character *character, char *selection) {
+
+    if (strcmp(selection, "Healing Aura") == 0) {
+        system("clear");
+        if (character->heal_spell->spell_name != NULL) {
+            free(character->heal_spell->spell_name);
+        }
+        character->heal_spell->spell_name = malloc(strlen("Healing Aura") + 1);
+        if (character->heal_spell->spell_name == NULL) {
+            exit(1);
+        }
+        strcpy(character->heal_spell->spell_name, "Healing Aura");
+    }
+
+    if (strcmp(selection, "Healing Light House") == 0) {
+        system("clear");
+        if (character->heal_spell->spell_name != NULL) {
+            free(character->heal_spell->spell_name);
+        }
+        character->heal_spell->spell_name = malloc(strlen("Healing Light House") + 1);
+        if (character->heal_spell->spell_name == NULL) {
+            exit(1);
+        }
+        strcpy(character->heal_spell->spell_name, "Healing Light House");
+    }
+
+    if (strcmp(selection, "Heart Of Dragon") == 0) {
+        system("clear");
+        if (character->heal_spell->spell_name != NULL) {
+            free(character->heal_spell->spell_name);
+        }
+        character->heal_spell->spell_name = malloc(strlen("Heart Of Dragon") + 1);
+        if (character->heal_spell->spell_name == NULL) {
+            exit(1);
+        }
+        strcpy(character->heal_spell->spell_name, "Heart Of Dragon");
+    }
+
+    printf("%s changed the heal spell to %s\n", character->username, get_heal(character));
+}
+
+void select_heal_spell(Character *character) {
+    system("clear");
+
+    char *heal_spell[] = {"Healing Aura", "Healing Light House", "Heart Of Dragon"};
+    int quantity = sizeof(heal_spell) / sizeof(heal_spell[0]);
+
+    char *heal = get_heal(character);
+    printf("Your current heal spell : %s\n", heal);
+    printf("\n");
+
+    for (int i = 0; i < quantity; i++) {
+        printf("%d. %s\n", i + 1, heal_spell[i]);
+    }
+
+    printf("\n");
+
+    int choice;
+
+    printf("Enter your choice (or 0 to cancel): ");
+    scanf("%d", &choice);
+
+    system("clear");
+
+    if (choice >= 1 && choice <= quantity) {
+        // printf("%s", heal_spell[choice - 1]);
+        set_heal(character, heal_spell[choice - 1]);
+        heal = get_heal(character);
+    } else if (choice != 0) {
+        printf("invalid choice: %d\n", choice);
+    }
+
+    free(heal);
 }
 
 void choose_another_spell(Character *character){
@@ -207,6 +292,7 @@ void choose_another_spell(Character *character){
             break;
         case 3:
             system("clear");
+            select_heal_spell(character);
             break;
         default:
             system("clear");
